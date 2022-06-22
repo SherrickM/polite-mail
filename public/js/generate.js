c9ce54468f3bf763fa569241a73292bd6deda978
Marquis <smcneal89@gmail.com>
2022-06-21 20:58:12 -0400

    added generate.js and update option selection

---
+++ public/js/generate.js
@@ -0,0 +1,6 @@
+const firstName = document.getElementById("first-name");
+const recipientName = document.getElementById("recipient-name");
+const emailAddress = documnet.getElementById("email-address");
+const emailConcern = document.getElementById("email-concern");
+
+// const generateHTML = ;
