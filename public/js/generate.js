af089c82cbc8dd536c32f99c239753cb6f0d0552
Marquis <smcneal89@gmail.com>
2022-06-21 20:18:02 -0400

    added generate js, updated generate handlebar

---
+++ public/js/generate.js
@@ -0,0 +1,7 @@
+const firstName = document.getElementById("first-name");
+const recipientName = document.getElementById("recipient-name");
+const emailAddress = documnet.getElementById("email-address");
+const emailConcern = document.getElementById("email-concern");
+
+const generateHTML = ;
+
