9027a2a10a52fd796b072986f7d7bf14170fa540
Christiana <christianatroyon@gmail.com>
2022-06-23 09:45:14 -0400

    added template route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const templateRoutes = require('./templateRoute');
 
+router.use('/templates',templateRoutes);
 router.use('/users',userRoutes);
 
 
